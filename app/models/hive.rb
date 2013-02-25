89ede9832aa35d6f8520e07a5aaa2fd50fdf16d3
Heath Ritchie <heath@heathritchie.com>
2013-02-25 11:42:20 -0500

    Start new hives with 2 boxes.

--- app/models/hive.rb
+++ app/models/hive.rb
@@ -1,3 +1,15 @@
 class Hive < ActiveRecord::Base
   has_many :boxes
+  after_create :init
+
+  def add_box
+    Box.create(hive_id: self.id)
+  end
+
+  def init
+    2.times do
+      add_box
+    end
+  end
+
 end
