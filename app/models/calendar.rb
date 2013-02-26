a8378ff8e558ae282a9f3a3112d8806d9c127744
Heath Ritchie <heath@heathritchie.com>
2013-02-26 07:36:24 -0500

    Initialize new calendars at week = 1.

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,5 +1,10 @@
 class Calendar < ActiveRecord::Base
   belongs_to :hive
+  after_create :init
   validates :hive_id, presence: true
   attr_accessible :week, :hive_id
+
+  def init
+    self.week = 1
+  end
 end
